5f68a2e0637c4da2fe5df115cba5eb14d2f84691
Gabriel-RSales <ga.willian.sales@outlook.com>
2022-02-09 22:44:29 -0300

    update DB

--- backend/prisma/migrations/20211220191138_create_user/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "login" TEXT NOT NULL,
-    "github_id" INTEGER NOT NULL,
-    "avatar_url" TEXT NOT NULL,
-    "name" TEXT NOT NULL
-);
