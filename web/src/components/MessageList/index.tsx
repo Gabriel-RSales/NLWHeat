5a52b626d7bfb9dcc842dfbe15219f2303f2dce6
Gabriel-RSales <ga.willian.sales@outlook.com>
2022-02-26 01:22:56 -0300

    Create messageLoad

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -48,22 +48,70 @@ export function MessageList() {
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="DoWhile 2021" />
 
-            <ul className={styles.messageList}>
-                {messages.map(message => {
-                    return (
-                        <li key={message.id} className={styles.message}>
-                            <p className={styles.messageContent}>{message.text}</p>
-                            <div className={styles.messageUser}>
-                                <div className={styles.userImg}>
-                                    <img src={message.user.avatar_url} alt={message.user.login} />
+            {messages.length
+            
+                ?
+                <ul className={styles.messageList}>
+                    {messages.map(message => { 
+                        return (
+                            <li key={message.id} className={styles.message}>
+                                <p className={styles.messageContent}>{message.text}</p>
+                                <div className={styles.messageUser}>
+                                    <div className={styles.userImg}>
+                                        <img src={message.user.avatar_url} alt={message.user.login} />
+                                    </div>
+                                    <span>{message.user.login}</span>
                                 </div>
-                                <span>{message.user.login}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                :
+                <ul className={styles.messageListLoad}>
+                    <li className={styles.messageLoad}>
+                        <div className={styles.messageContentLoad}>
+                            <p>primeiro</p>
+                            <p>primeiro</p>
+                            <p>primeiro</p>
+                        </div>
+                        <div className={styles.messageUserLoad}>
+                            <div className={styles.userImgLoad}>
+                                <span>img</span>
                             </div>
-                        </li>
-                    );
-                })}
-                
-            </ul>
+                            <span>nome</span>
+                        </div>
+                    </li>
+
+                    <li className={styles.messageLoad}>
+                        <div className={styles.messageContentLoad}>
+                            <p>segundo</p>
+                            <p>segundo</p>
+                            <p>segundo</p>
+                        </div>
+                        <div className={styles.messageUserLoad}>
+                            <div className={styles.userImgLoad}>
+                                img
+                            </div>
+                            <span>nome</span>
+                        </div>
+                    </li>
+
+                    <li className={styles.messageLoad}>
+                        <div className={styles.messageContentLoad}>
+                            <p>terceiro</p>
+                            <p>terceiro</p>
+                            <p>terceiro</p>         
+                        </div>
+                        <div className={styles.messageUserLoad}>
+                            <div className={styles.userImgLoad}>
+                                <span>img</span>
+                            </div>
+                            <span>nome</span>
+                        </div>
+                    </li>
+                </ul>
+            }
         </div>
     )
 }
