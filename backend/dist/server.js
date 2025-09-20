cd799f3fed33136695aec50b989e9e216e9ccaa9
Gabriel-RSales <ga.willian.sales@outlook.com>
2025-09-19 23:58:16 -0300

    Remove npm dist

--- backend/dist/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = process.env.PORT || 3000;
-app_1.serverHttp.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
